cd9773c7ad3c438e21c71099c01b580b367a97f0
Lana Avery <memekoten@gmail.com>
2026-03-02 15:50:58 +1100

    Performance optimizations related to Odyssey shuttles

--- Source/FoodGrabbing.cs
+++ Source/FoodGrabbing.cs
@@ -12,66 +12,76 @@ namespace Meals_On_Wheels
 	[HarmonyPatch(typeof(FoodUtility), nameof(FoodUtility.TryFindBestFoodSourceFor))]
 	class FoodGrabbing
 	{
-		//public static bool TryFindBestFoodSourceFor(Pawn getter, Pawn eater, bool desperate, out Thing foodSource, out ThingDef foodDef, bool canRefillDispenser = true, bool canUseInventory = true, bool canUsePackAnimalInventory = false, bool allowForbidden = false, bool allowCorpse = true, bool allowSociallyImproper = false, bool allowHarvest = false, bool forceScanWholeMap = false, bool ignoreReservations = false, bool calculateWantedStackCount = false, FoodPreferability minPrefOverride = FoodPreferability.Undefined)
 		[HarmonyPriority(Priority.Low)]
 		public static void Postfix(ref bool __result, Pawn getter, Pawn eater, ref Thing foodSource, ref ThingDef foodDef, bool canUseInventory, bool canUsePackAnimalInventory)
 		{
-			if (eater.IsFreeColonist && __result == false && canUseInventory && canUsePackAnimalInventory &&
-				getter.RaceProps.ToolUser && getter.health.capacities.CapableOf(PawnCapacityDefOf.Manipulation))
+			if (__result || !eater.IsFreeColonist || !canUseInventory || !canUsePackAnimalInventory ||
+			    !getter.RaceProps.ToolUser || !getter.health.capacities.CapableOf(PawnCapacityDefOf.Manipulation))
 			{
 				Log.Message($"There be no food for " + eater);
-				var pawns = eater.Map.mapPawns.SpawnedPawnsInFaction(Faction.OfPlayer).FindAll(
-					p => p != getter &&
-					!p.Position.IsForbidden(getter) && 
-					getter.CanReach(p, PathEndMode.OnCell, Danger.Some)
-				);
-				foreach (var p in pawns)
-				{
-					Log.Message($"Food soon rotten on " + p + "?");
-					var thing = FoodUtility.BestFoodInInventory(p, eater, FoodPreferability.MealAwful);
-					if (thing != null && thing.TryGetComp<CompRottable>() is CompRottable compRottable &&
-						compRottable != null && compRottable.Stage == RotStage.Fresh && compRottable.TicksUntilRotAtCurrentTemp < GenDate.TicksPerDay / 2)
-					{
-						Log.Message($"Food is " + thing);
-						foodSource = thing;
-						foodDef = FoodUtility.GetFinalIngestibleDef(foodSource, false);
-						__result = true;
-						return;
-					}
-				}
-				foreach (var p in pawns)
-				{
-					Log.Message($"Food on " + p + "?");
-					var thing = FoodUtility.BestFoodInInventory(p, eater, FoodPreferability.DesperateOnly, FoodPreferability.MealLavish, 0f, !eater.IsTeetotaler());
-					if (thing != null)
-					{
-						Log.Message($"Food is " + thing);
-						foodSource = thing;
-						foodDef = FoodUtility.GetFinalIngestibleDef(foodSource, false);
-						__result = true;
-						return;
-					}
-				}
+				return;
+			}
 
-				var shuttles = eater.Map.listerThings.AllThings.OfType<Building_PassengerShuttle>().ToList();
-				shuttles.RemoveAll(x => x.Faction != Faction.OfPlayer);
-				foreach (var shuttle in shuttles)
-				{
-					var innerContainer = shuttle.TryGetComp<CompTransporter>()?.innerContainer;
-					for (int index = 0; index < innerContainer.Count; ++index)
-					{
-						Thing food = innerContainer[index];
-						if (food.def.IsNutritionGivingIngestible && food.IngestibleNow && eater.WillEat(food, eater, allowVenerated: false) && food.def.ingestible.preferability >= FoodPreferability.DesperateOnly && food.def.ingestible.preferability <= FoodPreferability.MealLavish && (!eater.IsTeetotaler() || !food.def.IsDrug) && (double)FoodUtility.NutritionForEater(eater, food) * (double)food.stackCount >= (double)0f)
-						{
-							innerContainer.TryDrop(food, ThingPlaceMode.Near, 1, out _);
+			var pawns = eater.Map.mapPawns.SpawnedPawnsInFaction(Faction.OfPlayer).FindAll(
+				p => p != getter &&
+				     !p.Position.IsForbidden(getter) && 
+				     getter.CanReach(p, PathEndMode.OnCell, Danger.Some) &&
+						 (p.inventory?.innerContainer?.Any() ?? false)
+			);
 
-							Log.Message($"Food is " + food);
-							foodSource = food;
-							foodDef = FoodUtility.GetFinalIngestibleDef(foodSource, false);
-							__result = true;
-							return;
-						}
-					}
+			foreach (var p in pawns)
+			{
+				Log.Message($"Food soon rotten on " + p + "?");
+				var thing = FoodUtility.BestFoodInInventory(p, eater, FoodPreferability.MealAwful);
+				if (thing?.TryGetComp<CompRottable>() is not
+				    { Stage: RotStage.Fresh, TicksUntilRotAtCurrentTemp: < GenDate.TicksPerDay / 2 }) continue;
+
+				Log.Message($"Food is " + thing);
+				foodSource = thing;
+				foodDef = FoodUtility.GetFinalIngestibleDef(foodSource, false);
+				__result = true;
+
+				return;
+			}
+
+			foreach (var p in pawns)
+			{
+				Log.Message($"Food on " + p + "?");
+				var thing = FoodUtility.BestFoodInInventory(p, eater, FoodPreferability.DesperateOnly, FoodPreferability.MealLavish, 0f, !eater.IsTeetotaler());
+				if (thing == null) continue;
+
+				Log.Message($"Food is " + thing);
+				foodSource = thing;
+				foodDef = FoodUtility.GetFinalIngestibleDef(foodSource, false);
+				__result = true;
+				return;
+			}
+
+			if (!ModsConfig.OdysseyActive)
+				return;
+
+			var shuttles = eater.Map.listerThings.ThingsOfDef(ThingDefOf.PassengerShuttle).Where(x => x.Faction == Faction.OfPlayer);
+			foreach (var shuttle in shuttles)
+			{
+				var innerContainer = shuttle.TryGetComp<CompTransporter>()?.innerContainer;
+				for (var index = 0; index < innerContainer.Count; ++index)
+				{
+					var food = innerContainer[index];
+					if (!food.def.IsNutritionGivingIngestible || !food.IngestibleNow ||
+					    !eater.WillEat(food, eater, allowVenerated: false) ||
+					    food.def.ingestible.preferability < FoodPreferability.DesperateOnly ||
+					    food.def.ingestible.preferability > FoodPreferability.MealLavish ||
+					    (eater.IsTeetotaler() && food.def.IsDrug) ||
+					    !(FoodUtility.NutritionForEater(eater, food) * (double)food.stackCount >= (double)0f)) continue;
+
+
+					innerContainer.TryDrop(food, ThingPlaceMode.Near, 1, out _);
+
+					Log.Message($"Food is " + food);
+					foodSource = food;
+					foodDef = FoodUtility.GetFinalIngestibleDef(foodSource, false);
+					__result = true;
+					return;
 				}
 			}
 		}
